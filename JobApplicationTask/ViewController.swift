1519ae4d2af19923255b13f088022c227928eaf6
Serdar Altındaş <serdaraltindas03@gmail.com>
2024-05-31 10:26:44 +0300

    Initial Commit

---
+++ JobApplicationTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JobApplicationTask
+//
+//  Created by Serdar Altındaş on 31.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
